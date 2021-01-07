31084eeef52ab1ea6ad2794aa0bed63ace702eae
ginus <ginus@qq.com>
2021-01-07 19:58:30 +0800

    增加jsdoc

--- behaviour/AutoSelect.js
+++ behaviour/AutoSelect.js
@@ -1,15 +1,20 @@
-//自动选中
 import m from "mithril";
-
-// 使用mithril路由判断选中
-export var AutoSelectByRoute = (route, button) => {
+/**
+ * 使用mithril路由判断选中
+ * @param {string} route mithril路由
+ * @param {object} button 菜单项按钮参数
+ * @returns object
+ */
+export let AutoSelectByRoute = (route, button) => {
   button.element = m.route.Link;
   button.selected = route === button.url.href;
   return button;
 };
-
-// 使用普通链接判断选中
-export var AutoSelectByHref = (button) => {
+/**
+ * 使用普通链接判断选中
+ * @param {object} button 菜单项按钮参数
+ */
+export let AutoSelectByHref = (button) => {
   button.selected = location.href === button.url.href;
   return button;
 };
--- storage/token.js
+++ storage/token.js
@@ -1,9 +1,17 @@
+/**
+ * 获取令牌
+ * @return {string} 令牌
+ */
 export let getToken = () => {
   let remembered = localStorage.getItem("auth_token_remembered");
   if (remembered) return localStorage.getItem("auth_token");
   return sessionStorage.getItem("auth_token");
 };
-
+/**
+ * 保存登录令牌
+ * @param {string} value 值
+ * @param {boolean} remember 是否记住
+ */
 export let setToken = (value, remember) => {
   if (remember) {
     localStorage.setItem("auth_token_remembered", true);
